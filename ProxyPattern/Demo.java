cdbba62fb1b7ff20d588e0779dabd92f4ed5ffba
zhangc47 <1228073191@qq.com>
2021-08-24 23:15:58 +0800

    1

---
+++ ProxyPattern/Demo.java
@@ -0,0 +1,37 @@
+package DesignPattern.ProxyPattern;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        int n = 0;
+        int v1 = 11;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入整数：");
+        int v110 =scanner.nextInt();
+
+        if (v110 == v1) {
+            System.out.println("==");
+            n++;
+            if (n >= 1) {
+                // break;
+            }
+
+        }
+        else {
+            System.out.println("!=");
+        }
+
+        System.out.println("end");
+    }
+
+
+
+
+
+}
+
+
+
+
---
+++ SimpleFactory/demo.java
@@ -0,0 +1,17 @@
+package DesignPattern.SimpleFactory;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class demo {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("K1", "V1");
+        map.put("K2", "V2");
+        map.put("K3", "V3");
+    }
+
+}
