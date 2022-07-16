5d1f609f7dd105d2fbb2dfe2aa8a5e2750ecf618
Zane47 <1228073191@qq.com>
2022-07-16 23:42:19 +0800

    单策略模式的问题

--- design-pattern/src/main/java/strategy/Test.java
+++ design-pattern/src/main/java/strategy/Test.java
@@ -4,11 +4,30 @@ package strategy;
  * Created by geely
  */
 public class Test {
-    public static void main(String[] args) {
+
+    /**
+     * 普通方法
+     */
+    /*public static void main(String[] args) {
         PromotionActivity promotionActivity618 = new PromotionActivity(new LiJianPromotionStrategy());
         PromotionActivity promotionActivity1111 = new PromotionActivity(new FanXianPromotionStrategy());
 
         promotionActivity618.executePromotionStrategy();
         promotionActivity1111.executePromotionStrategy();
-    }
+    }*/
+
+    /**
+     * 单策略模式, 还是无法消除ifelse -> 策略模式 + 工厂模式
+     */
+    /*public static void main(String[] args) {
+        PromotionActivity promotionActivity = null;
+        String promotionKey = "lijian";
+        if ("lijian".equalsIgnoreCase(promotionKey)) {
+            promotionActivity = new PromotionActivity(new LiJianPromotionStrategy());
+        } else if ("fanxian".equalsIgnoreCase(promotionKey)) {
+            promotionActivity = new PromotionActivity(new FanXianPromotionStrategy());
+        }// ....
+        promotionActivity.executePromotionStrategy();
+    }*/
+
 }
